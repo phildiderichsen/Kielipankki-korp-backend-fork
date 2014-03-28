0fa13170d369f8db59411ae85925eaf17a053a71
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2014-03-28 22:05:32 +0200

    korpexport.exporter: More formatting methods
    korpexport/exporter.py:
      Defined __all__.
      KorpExporter:
        _option_default_defaults: Added several format string options.
        make_download_file(), process_query(): Use debug logging.
        make_download_content(), format_sentence(): Use a format string in
          self._opts.
        format_header(), format_footer(): Use _format_header_footer().
        format_sentences(): Use _format_list().
        Added get_sentences(), get_sentence_corpus(), get_sentence_tokens(),
          get_sentence_tokens_match(), get_sentence_tokens_left_context(),
          get_sentence_tokens_right_context(), get_sentence_match_position(),
          get_aligned_sentences(), get_sentence_structs(),
          get_sentence_struct_values(), _format_list(), format_date(),
          format_params(), _format_header_footer(), format_aligned_sentences(),
          format_aligned_sentence(), format_stucts(), format_struct(),
          format_tokens(), format_token(), get_token_attrs(),
          format_token_attrs(), format_token_attr().
    
    korpexport/delimited.py:
      Defined __all__.
      KorpExporterDelimited:
        Added default _delimit_opts.
        format_sentence(): Use methods in exporter.
        Moved format_sentence_tokens(), format_token(), format_token_attrs() to
          exporter.
        format_fields(): Renamed from format_delimited_fields(). Replaced
          escape_quote with replace_quote in _delimit_opts.
      KorpExporterCSV, KorpExporterTSV:
        _delimit_opts: Replaced escape_quote with replace_quote.

--- korpexport/delimited.py
+++ korpexport/delimited.py
@@ -7,16 +7,22 @@ from __future__ import absolute_import
 from .exporter import KorpExporter
 
 
+__all__ = ['KorpExporterCSV', 'KorpExporterTSV']
+
+
 class KorpExporterDelimited(KorpExporter):
 
     _option_defaults = {}
-    _delimit_opts = {}
+    _delimit_opts = {"delimiter": u",",
+                     "quote": u"\"",
+                     "replace_quote": u"\"\"",
+                     "newline": u"\n"}
 
     def __init__(self, *args, **kwargs):
         KorpExporter.__init__(self, *args, **kwargs)
 
     def format_headings(self):
-        return self.format_delimited_fields(
+        return self.format_fields(
             ["corpus", "position", "left context", "match", "right context"]
             + (["aligned text"] if self.is_parallel_corpus() else [])
             + self._opts.get("structs", []))
@@ -32,61 +38,33 @@ class KorpExporterDelimited(KorpExporter):
         - tokens in right context, separated with spaces
         - for parallel corpora only: tokens in aligned sentence
         """
-        # Match start and end positions in tokens
-        match_start = sentence["match"]["start"]
-        match_end = sentence["match"]["end"]
-        fields = [sentence["corpus"],
-                  str(sentence["match"]["position"]),
-                  self.format_sentence_tokens(sentence["tokens"][:match_start]),
-                  self.format_sentence_tokens(sentence["tokens"]
-                                              [match_start:match_end]),
-                  self.format_sentence_tokens(sentence["tokens"][match_end:])]
-        if "aligned" in sentence:
-            for align_key, tokens in sorted(sentence["aligned"].iteritems()):
-                fields.append(self.format_sentence_tokens(tokens))
-        # Value may be None; convert them to empty strings
-        fields.extend(sentence["structs"].get(struct) or ""
-                      for struct in self._opts.get("structs", []))
-        return self.format_delimited_fields(fields)
+        fields = ([self.get_sentence_corpus(sentence),
+                   str(self.get_sentence_match_position(sentence))]
+                  + [self.format_tokens(field_get_method(sentence))
+                     for field_get_method in
+                     [self.get_sentence_tokens_left_context,
+                      self.get_sentence_tokens_match,
+                      self.get_sentence_tokens_right_context]])
+        for _, tokens in self.get_aligned_sentences(sentence):
+            fields.append(self.format_tokens(tokens))
+        fields.extend(self.get_sentence_struct_values(sentence))
+        return self.format_fields(fields)
 
-    def format_sentence_tokens(self, tokens):
-        """Format the tokens of a single sentence."""
-        return self._opts["token_separator"].join(
-            self.format_token(token) for token in tokens)
-
-    def format_token(self, token):
-        """Format a single token, possibly with attributes."""
-        # Allow for None in word (but where do they come from?)
-        result = self._opts["word_format"].format(
-            word=(token.get("word") or ""))
-        if self._opts.get("attrs"):
-            result = self._opts["token_format"].format(
-                word=result, attrs=self.format_token_attrs(token))
-        return result
-
-    def format_token_attrs(self, token):
-        """Format the attributes of a token."""
-        return self._opts["attr_separator"].join(
-            self._opts["attr_format"].format(name=attrname,
-                                             value=(token.get(attrname) or ""))
-            for attrname in self._opts["attrs"])
-
-    def format_delimited_fields(self, fields):
+    def format_fields(self, fields):
         """Format fields according to the options in self._delimit_opts.
 
         self._delimit_opts may contain the following keyword arguments:
         - delim: field delimiter (default: ,)
         - quote: quotes surrounding fields (default: ")
-        - escape_quote: string to precede a quote with in a field value to
-          escape it (default: ")
+        - replace_quote: string to replace a quote with in a field value to
+          escape it (default: "")
         - newline: end-of-record string (default: \r\n)
         """
-        delim = self._delimit_opts.get("delimiter", u",")
-        quote = self._delimit_opts.get("quote", u"\"")
-        escape_quote = self._delimit_opts.get("escape_quote", quote)
-        newline = self._delimit_opts.get("newline", u"\n")
-        return (delim.join((quote + field.replace(quote, escape_quote + quote)
-                            + quote)
+        delim = self._delimit_opts["delimiter"]
+        quote = self._delimit_opts["quote"]
+        replace_quote = self._delimit_opts["replace_quote"]
+        newline = self._delimit_opts["newline"]
+        return (delim.join((quote + field.replace(quote, replace_quote) + quote)
                            for field in fields)
                 + newline)
 
@@ -98,7 +76,7 @@ class KorpExporterCSV(KorpExporterDelimited):
     _filename_extension = ".csv"
     _delimit_opts = {"delimiter": u",",
                      "quote": u"\"",
-                     "escape_quote": u"\"",
+                     "replace_quote": u"\"\"",
                      "newline": u"\r\n"}
 
     def __init__(self, *args, **kwargs):
@@ -112,7 +90,7 @@ class KorpExporterTSV(KorpExporterDelimited):
     _filename_extension = ".tsv"
     _delimit_opts = {"delimiter": u"\t",
                      "quote": u"",
-                     "escape_quote": u"",
+                     "replace_quote": u"",
                      "newline": u"\r\n"}
 
     def __init__(self, *args, **kwargs):
--- korpexport/exporter.py
+++ korpexport/exporter.py
@@ -12,6 +12,9 @@ import urllib, urllib2
 import logging
 
 
+__all__ = ['make_download_file', 'KorpExportError', 'KorpExporter']
+
+
 def make_download_file(*args, **kwargs):
     """Format query results and return them in a downloadable format."""
     return KorpExporter.make_download_file(*args, **kwargs)
@@ -30,13 +33,28 @@ class KorpExporter(object):
     _filename_extension = ""
     _filename_base_default = "korp_kwic_"
     _option_defaults = {}
-    _option_default_defaults = {"headings": "",
-                                "word_format": u"{word}",
-                                "token_format": u"{word}[{attrs}]",
-                                "token_separator": u" ",
-                                "attr_format": u"{value}",
-                                "attr_separator": u";",
-                                "sentence_separator": ""}
+    _option_default_defaults = {
+        "headings": "",
+        "header_format": "{headings}",
+        "footer_format": "# {date}: {params}\n",
+        "word_format": u"{word}",
+        "token_format": u"{word}[{attrs}]",
+        "token_separator": u" ",
+        "attr_format": u"{value}",
+        "attr_separator": u";",
+        "sentence_format": (u"{corpus} {match_pos}: {left_context}"
+                            u"{match_open}{match}{match_close}"
+                            u"{right_context}\n"),
+        "sentence_separator": u"",
+        "aligned_format": u"{sentence}",
+        "aligned_separator": u" | ",
+        "struct_format": u"{name}: {value}",
+        "struct_separator": u"; ",
+        "text_format": u"{header}{sentences}{footer}",
+        "date_format": "%Y-%m-%d %H:%M:%S",
+        "params_format": (u"Corpora: {corpus}; CQP query: {cqp}; "
+                          u"start: {start}; end: {end}")
+        }
 
     def __init__(self, format_name, form, filename_base=None):
         self._format_name = format_name
@@ -52,14 +70,14 @@ class KorpExporter(object):
         """Format query results and return them in a downloadable format."""
         result = {}
         exporter = cls._get_exporter(form, **kwargs)
-        # logging.info('exporter: %s', exporter)
+        logging.debug('exporter: %s', exporter)
         exporter.process_query(korp_server_url)
         result["download_charset"] = exporter._download_charset
         result["download_content"] = (exporter.make_download_content()
                                       .encode(exporter._download_charset))
         result["download_content_type"] = exporter._mime_type
         result["download_filename"] = exporter.get_filename()
-        # logging.info('result: %s', result)
+        logging.debug('make_download_file result: %s', result)
         return result
 
     @classmethod
@@ -103,13 +121,14 @@ class KorpExporter(object):
                 self._query_params = json.loads(self._form.get("query_params"))
             else:
                 self._query_params = self._form
-            logging.info("query_params: %s", self._query_params)
+            logging.debug("query_params: %s", self._query_params)
             query_result_json = (
                 urllib2.urlopen(korp_server_url,
                                 urllib.urlencode(self._query_params))
                 .read())
         self._query_result = json.loads(query_result_json)
         self._opts = self._extract_options()
+        logging.debug("opts: %s", self._opts)
 
     def _extract_options(self):
         """Extract formatting options from form, affected by query_params."""
@@ -151,34 +170,175 @@ class KorpExporter(object):
             self._filename_base + time.strftime("%Y%m%d%H%M%S")
             + self._filename_extension)
 
+    def get_sentences(self):
+        return self._query_result["kwic"]
+
+    def get_sentence_corpus(self, sentence):
+        return sentence["corpus"]
+
+    def get_sentence_tokens(self, sentence, start, end):
+        return sentence["tokens"][start:end]
+
+    def get_sentence_tokens_match(self, sentence):
+        return self.get_sentence_tokens(sentence, sentence["match"]["start"],
+                                        sentence["match"]["end"])
+
+    def get_sentence_tokens_left_context(self, sentence):
+        return self.get_sentence_tokens(sentence, None,
+                                        sentence["match"]["start"])
+
+    def get_sentence_tokens_right_context(self, sentence):
+        return self.get_sentence_tokens(sentence, sentence["match"]["end"],
+                                        None)
+
+    def get_sentence_match_position(self, sentence):
+        return sentence["match"]["position"]
+
+    def get_aligned_sentences(self, sentence):
+        return sorted(sentence.get("aligned", {}).iteritems())
+
+    def get_sentence_structs(self, sentence):
+        # Value may be None; convert them to empty strings
+        return [(struct, sentence["structs"].get(struct) or "")
+                for struct in self._opts.get("structs", [])]
+
+    def get_sentence_struct_values(self, sentence):
+        return [value for name, value in self.get_sentence_structs(sentence)]
+
     def is_parallel_corpus(self):
         # FIXME: This does not work if the script gets the query result
         # from frontend instead of redoing the query, since the frontend
         # has processed the corpus names not to contain the vertical bar.
         return "|" in self._query_result["kwic"][0]["corpus"]
 
+    def _format_list(self, list_, type_name, format_fn=None):
+        format_fn = format_fn or getattr(self, "format_" + type_name)
+        return self._opts[type_name + "_separator"].join(
+            format_fn(elem) for elem in list_)
+
+    def _format_part(self, format_name, arg_fn_args, **format_arg_fns):
+        # A non-tested, non-used formatting function with a kind of
+        # lazy evaluation of format arguments: only those arguments
+        # are evaluated which occur in the format string.
+        format_str = self._opts[format_name + "_format"]
+
+        def make_format_arg_value(arg_name, arg_fn):
+            return (arg_fn(*arg_fn_args) if "{" + arg_name + "}" in format_str
+                    else "")
+
+        format_args = [(name, make_format_arg_value(name, format_arg_fn))
+                       for (name, format_arg_fn) in format_arg_fns.iteritems()]
+        return format_str.format(**format_args)
+
     def make_download_content(self):
-        return (self.format_header()
-                + self.format_sentences()
-                + self.format_footer())
+        return self._opts["text_format"].format(
+            params=self.format_params(),
+            # Also allow format references {param[name]}
+            param=self._query_params,
+            date=self.format_date(),
+            header=self.format_header(),
+            sentences=self.format_sentences(),
+            footer=self.format_footer())
+
+    def format_date(self):
+        return time.strftime(self._opts["date_format"])
+
+    def format_params(self):
+        # Allow format references {name} as well as {param[name]}
+        return self._opts["params_format"].format(
+            param=self._query_params,
+            **self._query_params)
 
     def format_header(self):
-        if self._opts["headings"]:
-            return self.format_headings()
+        return self._format_header_footer("header")
+
+    def format_footer(self):
+        return self._format_header_footer("footer")
+
+    def _format_header_footer(self, type_):
+        headings = self.format_headings() if self._opts["headings"] else ""
+        return self._opts[type_ + "_format"].format(
+            headings=headings,
+            params=self.format_params(),
+            # Also allow format references {param[name]}
+            param=self._query_params,
+            date=self.format_date())
 
     def format_headings(self):
         return ""
 
     def format_sentences(self):
-        return self._opts["sentence_separator"].join(
-            self.format_sentence(sentence)
-            for sentence in self._query_result["kwic"])
+        return self._format_list(self.get_sentences(), "sentence")
 
     def format_sentence(self, sentence):
-        return ""
+        return self._opts["sentence_format"].format(
+            corpus=self.get_sentence_corpus(sentence),
+            position=self.get_sentence_match_position(sentence),
+            tokens=self.format_tokens(
+                self.get_sentence_tokens(sentence, None, None)),
+            match=self.format_tokens(
+                self.get_sentence_tokens_match(sentence)),
+            left_context=self.format_tokens(
+                self.get_sentence_tokens_left_context(sentence)),
+            right_context=self.format_tokens(
+                self.get_sentence_tokens_right_context(sentence)),
+            aligned=self.format_aligned_sentences(sentence),
+            structs=self.format_structs(sentence))
 
-    def format_footer(self):
-        return ""
+    def format_aligned_sentences(self, sentence):
+        return self._format_list(self.get_aligned_sentences(sentence),
+                                 "aligned", self.format_aligned_sentence)
+
+    def format_aligned_sentence(self, aligned_sentence):
+        align_key, sentence = aligned_sentence
+        return self._opts["aligned_format"].format(
+            align_key=align_key,
+            sentence=sentence)
+
+    def format_structs(self, sentence):
+        return self._format_list(self.get_sentence_structs(sentence), "struct")
+
+    def format_struct(self, struct):
+        return self._opts["struct_format"].format(
+            name=struct[0],
+            value=struct[1])
+
+    def format_tokens(self, tokens):
+        """Format the tokens of a single sentence."""
+        return self._format_list(tokens, "token")
+
+    def format_token(self, token):
+        """Format a single token, possibly with attributes."""
+        # Allow for None in word (but where do they come from?)
+        result = self._opts["word_format"].format(
+            word=(token.get("word") or ""))
+        if self._opts.get("attrs"):
+            result = self._opts["token_format"].format(
+                word=result, attrs=self.format_token_attrs(token))
+        return result
+
+    def get_token_attrs(self, token):
+        return [(attrname, token.get(attrname) or "")
+                for attrname in self._opts["attrs"]]
+
+    def format_token_attrs(self, token):
+        """Format the attributes of a token."""
+        return self._opts["attr_separator"].join(
+            self._opts["attr_format"].format(name=attrname,
+                                             value=(token.get(attrname) or ""))
+            for attrname in self._opts["attrs"])
+
+    def format_token_attrs(self, token):
+        """Format the attributes of a token."""
+        return self._format_list(self.get_token_attrs(token), "attr",
+                                 self.format_token_attr)
+
+    def format_token_attr(self, attr_name_value):
+        attrname, value = attr_name_value
+        return self._opts["attr_separator"].join(
+            self._opts["attr_format"].format(name=attrname,
+                                             value=(token.get(attrname) or ""))
+            for attrname in self._opts["attrs"])
 
 
 if __name__ == "__main__":
