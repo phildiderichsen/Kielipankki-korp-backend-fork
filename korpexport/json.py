7ac5c02ff5cae32ddfed479380ebdb0b8b4eaed8
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2014-03-31 14:53:31 +0300

    korpexport.json: Options for sort_keys and indent
    korpexport/json.py:
      KorpExporterJSON:
        Added _option_defaults with default values for sort_keys and indent.
        Renamed make_download_content() as format_content().
        format_content(): Use the option values for sort_keys and indent.

--- korpexport/json.py
+++ korpexport/json.py
@@ -14,10 +14,16 @@ class KorpExporterJSON(KorpExporter):
     _formats = ['json']
     _mime_type = 'application/json'
     _filename_extension = '.json'
+    _option_defaults = {
+        "sort_keys": "True",
+        "indent": "4"
+        }
 
     def __init__(self, *args, **kwargs):
         KorpExporter.__init__(self, *args, **kwargs)
 
-    def make_download_content(self):
+    def format_content(self):
         """Convert query_result to JSON."""
-        return json.dumps(self._query_result["kwic"], sort_keys=True, indent=4)
+        return json.dumps(self._query_result["kwic"],
+                          sort_keys=self._get_option_bool("sort_keys"),
+                          indent=self._get_option_int("indent"))
