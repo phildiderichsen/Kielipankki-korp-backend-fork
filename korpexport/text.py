a8755e3e40c05a40a9af04156eadee990af35768
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2014-04-01 16:40:25 +0300

    Added korpexport.text: Export plain text
    Added korpexport/text.py:
      Class KorpExporterText: A simple plain-text exporter.

---
+++ korpexport/text.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from __future__ import absolute_import
+
+from .exporter import KorpExporter
+
+
+class KorpExporterText(KorpExporter):
+
+    _formats = ["text"]
+    _mime_type = "text/plain"
+    _filename_extension = ".txt"
+    _option_defaults = {
+        "header_format": u"# {date}: {params}\n",
+        "footer_format": "",
+        "sentence_format": (u"{corpus} [{match_pos}]: {left_context}"
+                            u"{match_open}{match}{match_close}"
+                            u"{right_context} || {structs}\n"),
+        "match_open": " <<< ",
+        "match_close": " >>> "}
+
+    def __init__(self, *args, **kwargs):
+        KorpExporter.__init__(self, *args, **kwargs)
