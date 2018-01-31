83be0e39ee966427027da1c03352af3bc67190a0
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2018-01-31 15:30:00 +0200

    korplogtsv-get-query-corpora: Decode corpus list
    adminutil/korplogtsv-get-query-corpora:
      Decode the value of the parameter "corpus" using
      korplog_util.decode_list_param, supporting compression based on common
      prefixes.

--- adminutil/korplog_util.py
+++ adminutil/korplog_util.py
@@ -7,6 +7,7 @@ backend log data.
 """
 
 
+import re
 import time
 
 
@@ -23,3 +24,35 @@ def make_logentry_id(fields):
             pid=int(fields.get('pid', '0')))
     except ValueError:
         return 'None'
+
+
+def decode_list_param(str_list):
+    """Decode a list-valued parameter str_list into a list of strings.
+
+    Split str_list at comma and full stop. Also expand one level of
+    common prefixes with suffixes marked with parentheses:
+    LAM_A(HLA,NTR) -> LAM_AHLA,LAM_ANTR (nesting parentheses is not
+    allowed).
+    """
+    # Copied from korp.cgi, slightly modified.
+    split_val = re.split(r"[.,]", str_list)
+    result = []
+    prefix = ""
+    for elem in split_val:
+        # print elem, prefix, result
+        mo = re.match(r"^([^()]*)([()])?(.*)$", elem)
+        pref, sep, suff = mo.groups()
+        # print prefix, repr([pref, sep, suff])
+        if sep == "(":
+            # new_prefix(suffix
+            prefix = pref
+            result.append(prefix + suff)
+        elif sep == ')':
+            # last_suffix)
+            result.append(prefix + pref)
+            prefix = ""
+        else:
+            # Neither ( nor )
+            result.append(prefix + pref)
+    # print result
+    return result
