fd853c528ac78e9fd9242a5efe9f98a215c9ddad
Philip Diderichsen <philip.diderichsen@gmail.com>
2023-05-26 11:36:52 +0200

    Add setup.py to make korpexport an installable package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='korpexport',
+    version='0.0.1',
+    url='https://github.com/phildiderichsen/Kielipankki-korp-backend-fork',
+    author='Philip Diderichsen',
+    author_email="cpd@hum.ku.dk",
+    description="Fork of Jyrki's Korp exporter",
+    packages=['korpexport', 'korpexport.format']
+)
